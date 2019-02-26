ba0ced499b9f42e2d9d3cf5f144af5b54043a123
William Verde <williamverde10@gmail.com>
2019-02-26 15:32:04 -0400

    files lang

---
+++ Resources/lang/es/icommercecheckmos.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'single' => 'Check money order',
+    'description' => 'La descripcion del Modulo',
+    'list resource' => 'List icommercecheckmos',
+    'create resource' => 'Create icommercecheckmos',
+    'edit resource' => 'Edit icommercecheckmos',
+    'destroy resource' => 'Destroy icommercecheckmos',
+    'title' => [
+        'icommercecheckmos' => 'IcommerceCheckmo',
+        'create icommercecheckmo' => 'Create a icommercecheckmo',
+        'edit icommercecheckmo' => 'Edit a icommercecheckmo',
+    ],
+    'button' => [
+        'create icommercecheckmo' => 'Create a icommercecheckmo',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
