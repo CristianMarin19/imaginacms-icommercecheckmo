3f0910d4bdd1d7daba0394af92130b704ec0cddf
William Verde <williamverde10@gmail.com>
2021-01-13 16:26:53 -0400

    +Fix: bug exception

--- Http/Controllers/Api/IcommerceCheckmoApiController.php
+++ Http/Controllers/Api/IcommerceCheckmoApiController.php
@@ -75,7 +75,7 @@ class IcommerceCheckmoApiController extends BaseApiController
 
             // Validate minimum amount order
             if(isset($paymentMethod->options->minimunAmount) && $order->total<$paymentMethod->options->minimunAmount)
-              throw new Exception('Total order minimum not allowed', 204);
+              throw new \Exception('Total order minimum not allowed', 204);
 
             // get currency active
             $currency = $this->currency->getActive();
